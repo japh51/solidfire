030b629833a9af928b82754f46705326841dd4f6
Roberto <33968700+japh51@users.noreply.github.com>
2018-11-15 14:54:28 +0100

    Update snap_label_DR_0_1.py

--- snap_label_DR_0_1.py
+++ snap_label_DR_0_1.py
@@ -4,9 +4,10 @@ import re
 # Create connection to SF Cluster
 sfe = ElementFactory.create("192.168.13.207", "admin", "abc")
 
-
+# store the report of all snap and convert to string
 list_snap = str(sfe.list_snapshots())
 
+# regular expression to find in the string all of the snap id and FOR to modify all snap with snapmirror_label
 nums = re.findall('snapshot_id=(.+?),', list_snap)
 for num in nums:
     sfe.modify_snapshot(num, snap_mirror_label="DR")
